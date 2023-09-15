aeb81622172dda625f0b2a49f56f264ad655a16a
kimdohyung <pdohyung@gmail.com>
2023-09-16 01:45:54 +0900

    feat: Security formLogin 비활성화 및 BCryptPasswordEncoder 빈 등록

---
+++ src/main/kotlin/yourssu/blog/config/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package yourssu.blog.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain{
+
+        http.formLogin().disable()
+        http.csrf().disable()
+        http.cors().disable()
+
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+}
