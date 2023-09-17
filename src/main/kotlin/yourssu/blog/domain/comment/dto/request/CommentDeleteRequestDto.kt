2444009c90807e90d633920746a70fb06b40b626
kimdohyung <pdohyung@gmail.com>
2023-09-17 22:29:01 +0900

    feat: 댓글 삭제 기능 Request Dto 생성

---
+++ src/main/kotlin/yourssu/blog/domain/comment/dto/request/CommentDeleteRequestDto.kt
@@ -0,0 +1,11 @@
+package yourssu.blog.domain.comment.dto.request
+
+import javax.validation.constraints.NotBlank
+
+data class CommentDeleteRequestDto (
+        @field:NotBlank(message = "이메일을 입력해주세요.")
+        val email: String?,
+
+        @field:NotBlank(message = "패스워드를 입력해주세요.")
+        val password: String?
+)
