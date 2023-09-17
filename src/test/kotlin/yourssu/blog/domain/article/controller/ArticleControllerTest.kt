b750a1c663b6fdf89294176260c133dce895e50e
kimdohyung <pdohyung@gmail.com>
2023-09-17 16:30:45 +0900

    test: 게시글 수정 기능 테스트 코드 추가

--- src/test/kotlin/yourssu/blog/domain/article/controller/ArticleControllerTest.kt
+++ src/test/kotlin/yourssu/blog/domain/article/controller/ArticleControllerTest.kt
@@ -12,7 +12,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import yourssu.blog.domain.article.dto.request.ArticleCreateRequestDto
+import yourssu.blog.domain.article.dto.request.ArticleUpdateRequestDto
 import yourssu.blog.domain.article.dto.response.ArticleCreateResponseDto
+import yourssu.blog.domain.article.dto.response.ArticleUpdateResponseDto
 import yourssu.blog.domain.article.service.ArticleService
 
 class ArticleControllerTest : DescribeSpec() {
@@ -28,8 +30,8 @@ class ArticleControllerTest : DescribeSpec() {
         MockKAnnotations.init(this)
         mockMvc = MockMvcBuilders.standaloneSetup(ArticleController(articleService)).build()
 
-        describe("글 작성 요청 성공 시") {
-            it("responseDto와 201 status 반환한다.") {
+        describe("글 작성 요청") {
+            it("ArticleCreateResponseDto와 201 status 반환한다.") {
 
                 val request = ArticleCreateRequestDto(email = "test@test.com", password = "test-pw", title = "test", content = "test")
                 val response = ArticleCreateResponseDto(articleId = 1L, email = "test@test.com", title = "test", content = "test")
@@ -46,6 +48,28 @@ class ArticleControllerTest : DescribeSpec() {
                         .andExpect(jsonPath("$.email").value("test@test.com"))
                         .andExpect(jsonPath("$.title").value("test"))
                         .andExpect(jsonPath("$.content").value("test"))
+                        .andDo(print())
+            }
+        }
+
+        describe("글 수정 요청") {
+            it("ArticleUpdateResponseDto와 200 status 반환한다.") {
+
+                val request = ArticleUpdateRequestDto(email = "test@test.com", password = "test-pw", title = "title", content = "content")
+                val response = ArticleUpdateResponseDto(articleId = 1L, email = "test@test.com", title = "updatedTitle", content = "updatedContent")
+
+                every { articleService.update(any(), any()) } returns response
+
+                val result = mockMvc.perform(MockMvcRequestBuilders.patch("/api/articles/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                )
+
+                result.andExpect(status().isOk)
+                        .andExpect(jsonPath("$.articleId").value(1L))
+                        .andExpect(jsonPath("$.email").value("test@test.com"))
+                        .andExpect(jsonPath("$.title").value("updatedTitle"))
+                        .andExpect(jsonPath("$.content").value("updatedContent"))
                         .andDo(print())
             }
         }
--- src/test/kotlin/yourssu/blog/domain/article/service/ArticleServiceTest.kt
+++ src/test/kotlin/yourssu/blog/domain/article/service/ArticleServiceTest.kt
@@ -9,6 +9,7 @@ import io.mockk.mockk
 import io.mockk.verify
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import yourssu.blog.domain.article.dto.request.ArticleCreateRequestDto
+import yourssu.blog.domain.article.dto.request.ArticleUpdateRequestDto
 import yourssu.blog.domain.article.entity.Article
 import yourssu.blog.domain.article.repository.ArticleRepository
 import yourssu.blog.domain.user.entity.User
@@ -79,6 +80,60 @@ class ArticleServiceTest : BehaviorSpec() {
 
                 Then("성공") {
                     verify(exactly = 1) { articleRepository.save(any()) }
+                }
+            }
+        }
+
+        Given("글 수정") {
+            val user = User(email = "test@test.com", password = "test-pw", username = "test")
+            val request = ArticleUpdateRequestDto(email = "test@test.com", password = "test-pw", title = "updatedTitle", content = "updatedContent")
+            val article = Article(user = user, title = "title", content = "content")
+
+            When("존재하지 않는 이메일로 요청") {
+                every { userRepository.findByEmail(any()) } returns null
+
+                val result = shouldThrow<BusinessException> {
+                    userRepository.findByEmail(email = request.email!!)
+                            ?: throw BusinessException(ErrorCode.USER_NOT_FOUND)
+                }
+
+                Then("찾을 수 없는 사용자 예외처리") {
+                    result.errorCode.message shouldBe "해당 사용자를 찾을 수 없습니다."
+                }
+
+            }
+
+            When("일치하지 않는 패스워드로 요청") {
+
+                every { userRepository.findByEmail(any()) } returns user
+                every { passwordEncoder.matches(any(), any()) } returns false
+
+                val findUser = userRepository.findByEmail(request.email!!)!!
+
+                val result = shouldThrow<BusinessException> {
+                    if (!passwordEncoder.matches(request.password, findUser.password))
+                        throw BusinessException(ErrorCode.WRONG_PASSWORD)
+                }
+
+                Then("찾을 수 없는 사용자 예외처리") {
+                    result.errorCode.message shouldBe "잘못된 비밀번호입니다."
+                }
+
+            }
+
+            When("올바른 데이터로 요청") {
+                every { userRepository.findByEmail(any()) } returns user
+                every { passwordEncoder.matches(any(), any()) } returns true
+
+                val findUser = userRepository.findByEmail(request.email!!)!!
+
+                if (passwordEncoder.matches(request.password, findUser.password)) {
+                    article.update(request.title!!, request.content!!)
+                }
+
+                Then("성공") {
+                    article.title shouldBe "updatedTitle"
+                    article.content shouldBe "updatedContent"
                 }
             }
         }
