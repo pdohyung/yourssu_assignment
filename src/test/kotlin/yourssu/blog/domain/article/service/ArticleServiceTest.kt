2f104d8c5ffc7dbf55ce9788484e4220a947a426
kimdohyung <pdohyung@gmail.com>
2023-09-17 23:03:01 +0900

    test: 게시글 삭제 기능 테스트 코드 추가

--- src/test/kotlin/yourssu/blog/domain/article/service/ArticleServiceTest.kt
+++ src/test/kotlin/yourssu/blog/domain/article/service/ArticleServiceTest.kt
@@ -3,12 +3,11 @@ package yourssu.blog.domain.article.service
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
-import io.mockk.clearAllMocks
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import yourssu.blog.domain.article.dto.request.ArticleCreateRequestDto
+import yourssu.blog.domain.article.dto.request.ArticleDeleteRequestDto
 import yourssu.blog.domain.article.dto.request.ArticleUpdateRequestDto
 import yourssu.blog.domain.article.entity.Article
 import yourssu.blog.domain.article.repository.ArticleRepository
@@ -122,19 +121,109 @@ class ArticleServiceTest : BehaviorSpec() {
 
             }
 
-            When("올바른 데이터로 요청") {
-                every { userRepository.findByEmail(any()) } returns user
-                every { passwordEncoder.matches(any(), any()) } returns true
+            When("존재하지 않는 게시글로 요청") {
+                every { articleRepository.findByIdOrNull(any()) } returns null
 
-                val findUser = userRepository.findByEmail(request.email!!)!!
-
-                if (passwordEncoder.matches(request.password, findUser.password)) {
-                    article.update(request.title!!, request.content!!)
+                val result = shouldThrow<BusinessException> {
+                    articleRepository.findByIdOrNull(1L) ?: throw BusinessException(ErrorCode.ARTICLE_NOT_FOUND)
                 }
+
+                Then("찾을 수 없는 게시글 예외처리") {
+                    result.errorCode.message shouldBe "해당 게시글을 찾을 수 없습니다."
+                }
+            }
+
+            When("일치하지 않는 사용자로 요청") {
+                val anotherUser = User(email = "test1@test.com", password = "test-pw", username = "test")
+
+                val result = shouldThrow<BusinessException> {
+                    check(request.email == anotherUser.email) { throw BusinessException(ErrorCode.USER_NOT_MATCH) }
+                }
+
+                Then("일치하지 않은 사용자 예외처리"){
+                    result.errorCode.message shouldBe "사용자가 일치하지 않습니다."
+                }
+            }
+
+            When("올바른 데이터로 요청") {
+
+                article.update(request.title!!, request.content!!)
 
                 Then("성공") {
                     article.title shouldBe "updatedTitle"
                     article.content shouldBe "updatedContent"
+                }
+            }
+        }
+
+        Given("글 삭제") {
+            val user = User(email = "test@test.com", password = "test-pw", username = "test")
+            val request = ArticleDeleteRequestDto(email = "test@test.com", password = "test-pw")
+            val article = Article(user = user, title = "title", content = "content")
+
+            When("존재하지 않는 이메일로 요청") {
+                every { userRepository.findByEmail(any()) } returns null
+
+                val result = shouldThrow<BusinessException> {
+                    userRepository.findByEmail(email = request.email!!)
+                            ?: throw BusinessException(ErrorCode.USER_NOT_FOUND)
+                }
+
+                Then("찾을 수 없는 사용자 예외처리") {
+                    result.errorCode.message shouldBe "해당 사용자를 찾을 수 없습니다."
+                }
+
+            }
+
+            When("일치하지 않는 패스워드로 요청") {
+
+                every { userRepository.findByEmail(any()) } returns user
+                every { passwordEncoder.matches(any(), any()) } returns false
+
+                val findUser = userRepository.findByEmail(request.email!!)!!
+
+                val result = shouldThrow<BusinessException> {
+                    if (!passwordEncoder.matches(request.password, findUser.password))
+                        throw BusinessException(ErrorCode.WRONG_PASSWORD)
+                }
+
+                Then("찾을 수 없는 사용자 예외처리") {
+                    result.errorCode.message shouldBe "잘못된 비밀번호입니다."
+                }
+
+            }
+
+            When("존재하지 않는 게시글로 요청") {
+                every { articleRepository.findByIdOrNull(any()) } returns null
+
+                val result = shouldThrow<BusinessException> {
+                    articleRepository.findByIdOrNull(1L) ?: throw BusinessException(ErrorCode.ARTICLE_NOT_FOUND)
+                }
+
+                Then("찾을 수 없는 게시글 예외처리") {
+                    result.errorCode.message shouldBe "해당 게시글을 찾을 수 없습니다."
+                }
+            }
+
+            When("일치하지 않는 사용자로 요청") {
+                val anotherUser = User(email = "test1@test.com", password = "test-pw", username = "test")
+
+                val result = shouldThrow<BusinessException> {
+                    check(request.email == anotherUser.email) { throw BusinessException(ErrorCode.USER_NOT_MATCH) }
+                }
+
+                Then("일치하지 않은 사용자 예외처리"){
+                    result.errorCode.message shouldBe "사용자가 일치하지 않습니다."
+                }
+            }
+
+            When("올바른 데이터로 요청") {
+                every { articleRepository.deleteById(any()) } just runs
+
+                articleRepository.deleteById(1L)
+
+                Then("성공") {
+                    verify(exactly = 1) { articleRepository.deleteById(any()) }
                 }
             }
         }
