aeb6e3494fa1718f363de40a522387baa4f05c6c
kimdohyung <pdohyung@gmail.com>
2023-09-17 16:19:54 +0900

    feat: 게시글 수정 기능 로직 구현

--- src/main/kotlin/yourssu/blog/domain/article/service/ArticleService.kt
+++ src/main/kotlin/yourssu/blog/domain/article/service/ArticleService.kt
@@ -1,10 +1,13 @@
 package yourssu.blog.domain.article.service
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import yourssu.blog.domain.article.dto.request.ArticleCreateRequestDto
+import yourssu.blog.domain.article.dto.request.ArticleUpdateRequestDto
 import yourssu.blog.domain.article.dto.response.ArticleCreateResponseDto
+import yourssu.blog.domain.article.dto.response.ArticleUpdateResponseDto
 import yourssu.blog.domain.article.entity.Article
 import yourssu.blog.domain.article.repository.ArticleRepository
 import yourssu.blog.domain.user.repository.UserRepository
@@ -36,5 +39,25 @@ class ArticleService(
         val savedArticle = articleRepository.save(article)
 
         return ArticleCreateResponseDto(savedArticle.id, request.email, savedArticle.title, savedArticle.content)
+    }
+
+    @Transactional
+    fun update(articleId: Long, request: ArticleUpdateRequestDto): ArticleUpdateResponseDto {
+
+        val existUser = userRepository.findByEmail(request.email!!) ?: throw BusinessException(ErrorCode.USER_NOT_FOUND)
+
+        if(!passwordEncoder.matches(request.password, existUser.password)){
+            throw BusinessException(ErrorCode.WRONG_PASSWORD)
+        }
+
+        val findArticle = articleRepository.findByIdOrNull(articleId) ?: throw BusinessException(ErrorCode.ARTICLE_NOT_FOUND)
+
+        if(findArticle.user.email != request.email){
+            throw BusinessException(ErrorCode.USER_NOT_MATCH)
+        } else{
+            findArticle.update(request.title!!, request.content!!)
+        }
+
+        return ArticleUpdateResponseDto(findArticle.id, existUser.email, findArticle.title, findArticle.content)
     }
 }
