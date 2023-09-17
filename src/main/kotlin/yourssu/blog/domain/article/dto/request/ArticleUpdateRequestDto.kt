703841758ef68117ee4dea5ad9750bda9a05f06f
kimdohyung <pdohyung@gmail.com>
2023-09-17 16:11:08 +0900

    feat: 게시글 수정 기능의 Request, Response Dto 생성

---
+++ src/main/kotlin/yourssu/blog/domain/article/dto/request/ArticleUpdateRequestDto.kt
@@ -0,0 +1,18 @@
+package yourssu.blog.domain.article.dto.request
+
+import javax.validation.constraints.NotBlank
+
+data class ArticleUpdateRequestDto (
+
+        @field:NotBlank(message = "이메일을 입력해주세요.")
+        val email: String?,
+
+        @field:NotBlank(message = "패스워드를 입력해주세요.")
+        val password: String?,
+
+        @field:NotBlank(message = "제목을 입력해주세요.")
+        val title: String?,
+
+        @field:NotBlank(message = "내용을 입력해주세요.")
+        val content: String?
+)
